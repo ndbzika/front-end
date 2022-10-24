6e145b4f98456b956944c53cce62c7d06faf7b14
Flávio Henrique <chakcpps@gmail.com>
2022-10-24 08:14:05 -0300

    :zap: perf: Trocando funções
    Trocando funções onclick para cria-las no scipt e separando style dos botões para o style.css

--- calculadora/index.js
+++ calculadora/index.js
@@ -1,26 +1,6 @@
-function inserir(numero1)
-{
-    var numero2 = document.getElementById('display').innerHTML;
-    document.getElementById('display').innerHTML = numero2 + numero1
-}
-function limpar()
-{
-    document.getElementById('display').innerHTML = ""
-}
-function deletar()
-{
-    var res = document.getElementById('display').innerHTML
-    document.getElementById('display').innerHTML = res.substring(0, res.length -1)
-}
-function calc()
-{
-    var res = document.getElementById('display').innerHTML
-    if(res)
-    {
-        document.getElementById('display').innerHTML = eval(res)
-    }
-    else
-    {
-        document.getElementById('display').innerHTML = "Digite uma equação!"
-    }
+const display = document.querySelector("#display");
+
+function inserir(num){
+    const num1 = Number(num);
+    display.textContent = num1;
 }
